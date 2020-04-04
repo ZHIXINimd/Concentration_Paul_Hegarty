31fc310df3e9241e430d44839bb42b5d0bbb3a8d
Bruce <thewayofdragon1987@gmail.com>
2020-04-04 13:23:52 +1000

    Add flipCard function for a single button UI

--- Concentration_Paul_Hegarty/Control/ViewController.swift
+++ Concentration_Paul_Hegarty/Control/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    let emoji = "🤖"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func touchCard(_ sender: UIButton) {
+        
+        flipCard(withEmoji: emoji, on: sender)
+    }
+    
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji{
+            button.setTitle("", for: .normal)
+            button.backgroundColor = UIColor.orange
+        }else{
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
+    
 }
 
